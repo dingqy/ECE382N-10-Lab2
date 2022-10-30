9d54854af94a7864700e40d28e67d22dc8989749
Qiyang Ding <809393617@qq.com>
2022-10-30 16:46:44 -0500

    - Test update
    Test bug fix

--- sim/test.cpp
+++ sim/test.cpp
@@ -123,17 +123,17 @@ void init_test() {
             test_args[0].test_cases.emplace_back(0, test_case_t{true, 0x00000101, 0x100});
             test_args[2].test_cases.emplace_back(21, test_case_t{true, 0x00000101, 0x200});
 
-            test_args[1].test_records.emplace_back(31, test_record_t{false, 0x00000101});
-            test_args[1].test_goldens.emplace_back(31, test_result_t{INVALID, DIR_OWNED, 0x00000101, 0x0, 0x0, 0x0});
+            test_args[1].test_records.emplace_back(32, test_record_t{false, 0x00000101});
+            test_args[1].test_goldens.emplace_back(32, test_result_t{INVALID, DIR_OWNED, 0x00000101, 0x0, 0x0, 0x0});
 
-            test_args[0].test_records.emplace_back(41, test_record_t{true, 0x00010001});
-            test_args[0].test_goldens.emplace_back(41,
+            test_args[0].test_records.emplace_back(42, test_record_t{true, 0x00000101});
+            test_args[0].test_goldens.emplace_back(42,
                                                    test_result_t{INVALID, DIR_INVALID, 0x00000101, 0x100, 0x0, 0x0});
 
-            test_args[1].test_records.emplace_back(52, test_record_t{false, 0x00000101});
-            test_args[1].test_goldens.emplace_back(52, test_result_t{INVALID, DIR_OWNED, 0x00000101, 0x0, 0x0, 0x2});
-            test_args[2].test_records.emplace_back(53, test_record_t{true, 0x00000101});
-            test_args[2].test_goldens.emplace_back(53,
+            test_args[1].test_records.emplace_back(53, test_record_t{false, 0x00000101});
+            test_args[1].test_goldens.emplace_back(53, test_result_t{INVALID, DIR_OWNED, 0x00000101, 0x0, 0x0, 0x2});
+            test_args[2].test_records.emplace_back(54, test_record_t{true, 0x00000101});
+            test_args[2].test_goldens.emplace_back(54,
                                                    test_result_t{MODIFIED, DIR_INVALID, 0x00000101, 0x200, 0x0, 0x0});
             break;
         case 9:
