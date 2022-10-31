d74eacb94b60ad392d25cd1d0ef017061153bfb1
Qiyang Ding <809393617@qq.com>
2022-10-31 11:29:22 -0500

    - Test update
    Test 12 update

--- sim/test.cpp
+++ sim/test.cpp
@@ -185,14 +185,10 @@ void init_test() {
             test_args[2].test_cases.emplace_back(100, test_case_t{true, 0x00000101, 0x200});
 
             test_args[1].test_records.emplace_back(111, test_record_t{false, 0x00000101});
-            test_args[1].test_goldens.emplace_back(111, test_result_t{INVALID, DIR_OWNED, 0x00000101, 0x100, 0x3, 0x2});
+            test_args[1].test_goldens.emplace_back(111, test_result_t{INVALID, DIR_OWNED, 0x00000101, 0x100, 0x1, 0x2});
 
             test_args[2].test_records.emplace_back(121, test_record_t{true, 0x00000101});
             test_args[2].test_goldens.emplace_back(121,
-                                                   test_result_t{MODIFIED, DIR_INVALID, 0x00000101, 0x100, 0x0, 0x0});
-
-            test_args[2].test_records.emplace_back(122, test_record_t{true, 0x00000101});
-            test_args[2].test_goldens.emplace_back(122,
                                                    test_result_t{MODIFIED, DIR_INVALID, 0x00000101, 0x200, 0x0, 0x0});
 
             test_args[1].test_records.emplace_back(132, test_record_t{true, 0x00000101});
