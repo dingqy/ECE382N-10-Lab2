f4140238580c6ea362f4b93523c6e85bad350f42
Qiyang Ding <809393617@qq.com>
2022-11-01 16:28:32 -0500

    - Test infra update
    Test infra update

---
+++ sim/proc_submission.cpp
@@ -0,0 +1,225 @@
+// proc.cpp
+//   by Derek Chiou
+//      March 4, 2007
+// 
+
+// STUDENTS: YOU ARE EXPECTED TO MODIFY THIS FILE TO INSERT YOUR
+// TESTS.  HOWEVER, YOU MUST BE ABLE TO ACCEPT OTHER PROC.CPP FILES,
+// AS I WILL BE REPLACING YOUR PROC.CPP WITH MINE (AND YOUR FELLOW
+// STUDENTS') FOR TESTING PURPOSES.
+
+// for 382N-10
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "generic_error.h"
+#include "cache.h"
+#include "proc.h"
+#include "test.h"
+#include "helpers.h"
+
+/**
+ * Constructor of processor
+ *
+ * @param __p Processor ID
+ */
+proc_t::proc_t(int __p) {
+    proc = __p;
+    init();
+}
+
+/**
+ * Initialize the args in processor class
+ */
+void proc_t::init() {
+    response.retry_p = false;
+    ld_p = false;
+}
+
+/**
+ * Bind related cache
+ *
+ * @param c cache object
+ */
+void proc_t::bind(cache_t *c) {
+    cache = c;
+}
+
+
+// ***** FYTD ***** 
+
+// this is just a simple random test.  I'm not giving
+// you any more test cases than this.  You will be tested on the
+// correctness and performance of your solution.
+
+/**
+ * Simulate processor memory access behavior
+ *
+ * Test cases description
+ *
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 1:
+ *  - Directory Invalid -> Read request -> Exclusive in cache and Owned in directory (Local)
+ *
+ * Case 2:
+ *  - Directory Invalid -> Read request -> Network -> Node reply -> Exclusive in cache and Owned in directory (global)
+ *
+ * Case 3:
+ *  - Directory Invalid -> Write request -> Modified in cache and Owned in directory (Local)
+ *
+ * Case 4:
+ *  - Directory Invalid -> Write request -> Network -> Node reply -> Exclusive in cache and Owned in directory (global)
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 5:
+ *  - Directory Owned (Global) -> Read request -> Forward request -> Network -> Node reply (To directory and source) -> Shared in both caches and directory
+ *
+ * Case 6:
+ *  - Directory Owned (Local) -> Invalidate request (Write back) -> Copy data and Invalid state in directory
+ *
+ * Case 7:
+ *  - Directory Owned (Global) -> Write request (Write back) -> Copy data and Invalid state in directory
+ *
+ * Case 8:
+ *  - Directory Owned (Global) -> Write request -> Forward request -> Network -> Node reply (To directory and source) -> Modified in one cache, Invalid in other cache, and owner change in directory
+ *
+ * Case 9:
+ *  - Directory Owned (Local) -> Read request -> Forward request -> Network -> Node reply (To directory and source) -> Shared in both caches and directory
+ *
+ * Case 10:
+ *  - Directory Owned (Local) -> Write request -> Forward request -> Network -> Node reply (To directory and source) -> Modified in one cache, Invalid in other cache, and owner change in directory
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 11:
+ *  - Directory Shared (Global) -> Read request -> Node reply (To source) -> Shared in cache and sharer list update in directory
+ *
+ * Case 12:
+ *  - Directory Shared (Global) -> Write request -> Invalidation broadcast -> Node reply (To source) -> Invalidation ack -> Modified in cache, invalidation, and owned in directory
+ *
+ * Case 13:
+ *  - Directory Shared (Global) -> Write request (write back) -> Update sharer list
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 14:
+ *  - Directory Shared-no-data -> Read request -> Forward request -> Node reply (To directory and source) -> Shared in cache and sharer list update
+ *
+ * Case 15:
+ *  - Directory Shared-no-data -> Write request (write back) -> Invalid in directory
+ *  -                                           Read request -> Forward request -> Retry/Resubmit -> Exclusive in cache and owned in directory
+ *
+ * Case 16:
+ *  - Directory shared-no-data -> Write request -> Resubmit -> Directory shared -> invalidation -> Modified in cache and owned in directory
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 17:
+ *  - Directory Shared (Local) -> Read request -> Node reply (To source) -> Shared in cache and sharer list update in directory
+ *
+ * Case 18:
+ *  - Directory Shared (Local) -> Write request -> Invalidation broadcast -> Node reply (To source) -> Invalidation ack -> Modified in cache, invalidation, and owned in directory
+ *
+ * Case 19:
+ *  - Directory Shared (Local) -> Write request (write back) -> Update sharer list
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 20:
+ *  - Directory Shared (Global) -> Write request -> Invalidation -> ...
+ *  -                                               Write request -> Resubmit/Retry
+ *
+ * Case 21:
+ *  - Directory Shared (Global) -> Write request -> Invalidation -> ...
+ *  -                                               Write request -> Forward request
+ *
+ * Case 22:
+ *  - Directory Shared (Local) -> Write request -> Invalidation -> ...
+ *  -                                               Write request -> Forward request
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 23:
+ *  - Directory Shared-no-data (Local) -> Read request -> Forward request -> Node reply (To directory and source) -> Shared in cache and sharer list update
+ *
+ * Case 24:
+ *  - Directory shared-no-data (Local) -> Write request -> Resubmit -> Directory shared -> invalidation -> Modified in cache and owned in directory
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 25:
+ *  - Invalidation 31 nodes (Global)
+ *
+ * Case 26:
+ *  - Invalidation 31 nodes (Local)
+ *
+ * Case 27:
+ *  - Directory Shared (Global) -> Write request -> Invalidation
+ *                                                  Read request -> Retry/Resubmit
+ *
+ * Case 28:
+ *  - Queue 2 (Invalidation queue full)
+ *  - Ownership and directory node same (Owned -> Shared-No-data -> Shared)
+ *
+ * Case 29:
+ *  - Directory shared (Global) -> Write request -> Invalidation          -> Node non-ack (Cache miss)    -> No more action
+ *  -                                        Write back (Shared) -> Directory update sharer list
+ * --------------------------------------------------------------------------------------------------------------
+ * Case 30:
+ *  - Random
+ *
+ * Case 31:
+ *  - Directory Owned (with owner) -> Read request -> Node reply (Memory write) -> Shared in both caches
+ *
+ * Case 32:
+ *  - Directory Shared (Local) -> Write request -> Invalidation
+ *                                                  Read request -> Retry/Resubmit
+ * --------------------------------------------------------------------------------------------------------------
+ */
+void proc_t::advance_one_cycle() {
+    int data;
+
+    if (init_valid) {
+        for (auto &i: test_args[proc].test_inits) {
+            cache->set_mem(i.address, i.data);
+        }
+        init_valid = false;
+    }
+
+    test_args_t test_set = test_args[proc];
+
+    // TODO: ADD test behavior here
+    switch (args.test) {
+        case 0:
+            if (!response.retry_p) {
+                addr = random() % test_args[proc].addr_range;
+                ld_p = ((random() % 2) == 0);
+            }
+            if (ld_p) response = cache->load(addr, 0, &data, response.retry_p);
+            else response = cache->store(addr, 0, cur_cycle, response.retry_p);
+            break;
+        case 1:
+            if (case_index < test_set.test_cases.size()) {
+                if (cur_cycle >= test_set.test_cases[case_index].first) {
+                    if (test_set.test_cases[case_index].second.write) {
+                        response = cache->store(test_set.test_cases[case_index].second.address, 0,
+                                                test_set.test_cases[case_index].second.data,
+                                                response.retry_p);
+                    } else {
+                        response = cache->load(test_set.test_cases[case_index].second.address, 0, &data,
+                                               response.retry_p);
+                        if (!response.retry_p) {
+                            test_set.test_results.emplace_back(cur_cycle, test_result_t{INVALID, DIR_INVALID,
+                                                                                        test_set.test_cases[case_index].second.address,
+                                                                                        data, 0x0, 0x0});
+                        }
+                    }
+                    if (!response.retry_p) {
+                        case_index += 1;
+                    }
+                }
+            }
+            break;
+        case 2:
+            if (!response.retry_p) {
+                std::uniform_int_distribution<int> distribution{0, test_args[proc].addr_range};
+                addr = distribution(test_args[proc].random_generator);
+                ld_p = ((random() % 2) == 0);
+                std::cout << "Cycle: " << cur_cycle << ", Node: " << proc << ", Address: " << addr << ", Load: " << ld_p << std::endl;
+            }
+            if (ld_p) response = cache->load(addr, 0, &data, response.retry_p);
+            else response = cache->store(addr, 0, cur_cycle, response.retry_p);
+            break;
+        default: ERROR("don't know this test case");
+    }
+}
---
+++ sim/test_submission.cpp
@@ -0,0 +1,83 @@
+// test.cpp
+//   Derek Chiou
+//     May 19, 2007
+
+// STUDENTS: YOU ARE EXPECTED TO PUT YOUR TESTS IN THIS FILE, ALONG WITH PROC.CPP.
+
+#include <stdio.h>
+#include <iostream>
+#include "types.h"
+#include "generic_error.h"
+#include "helpers.h"
+#include "cache.h"
+#include "test.h"
+
+extern args_t args;
+extern int addr_range;
+extern cache_t **caches;
+
+test_args_t test_args[32];
+
+/**
+ * Initialize the test address range
+ *
+ * TODO: ADD test here
+ */
+void init_test() {
+    switch (args.test) {
+        case 0:
+            test_args[0].addr_range = 1024;
+            break;
+        case 1:
+            break;
+        case 2:
+            for (int i = 0; i < 32; i++) {
+                test_args[i].random_generator.seed(i * 10);
+                test_args[i].addr_range = 32767;
+            }
+            break;
+        default: ERROR("don't recognize this test");
+    }
+}
+
+/**
+ * Deal with the performance of the test
+ *
+ * TODO: ADD test here
+ */
+void finish_test() {
+    double hr;
+
+    std::cout << "-------------------------------------------------------" << std::endl;
+    for (int i = 0; i < args.num_procs; ++i) {
+        switch (args.test) {
+            case 0:
+                hr = caches[i]->hit_rate();
+                if (!within_tolerance(hr, 0.5, 0.001)) {
+                    ERROR("out of tolerance");
+                }
+                break;
+            case 1:
+            case 2:
+                std::cout << "Processor: " << i << " Checking..." << std::endl;
+                if (test_args[i].test_goldens.size() != test_args[i].test_results.size()) {
+                    ERROR("Test numbers are different.")
+                }
+                for (int j = 0; j < test_args[i].test_goldens.size(); j++) {
+                    std::cout << "Processor: " << i << " Checking record " << j << std::endl;
+                    test_result_t test_result = test_args[i].test_results[j].second;
+                    test_result_t test_golden = test_args[i].test_goldens[j].second;
+                    if (test_result.data != test_golden.data) {
+                        std::cout << "Cycle: " << test_args[i].test_results[j].first << ", Data result: "
+                                  << test_result.data << ", Data expected: " << test_golden.data << std::endl;
+                        ERROR("Test data error")
+                    }
+                }
+                std::cout << "Passed" << std::endl;
+                break;
+            default: ERROR("don't recognize this test");
+        }
+        std::cout << "-------------------------------------------------------" << std::endl;
+    }
+    printf("passed\n");
+}
